a97097313d4987b40956a0fb8cec0d6093b2b2c5
Steve Zhao <>
2026-01-14 13:13:16 -0500

    day2_calculator2 final

--- day2_calculator2.py
+++ day2_calculator2.py
@@ -6,26 +6,27 @@ def testnumber(tester):
             return number
         except ValueError:
             print("你输入的不是数字哦，请输入数字：")
+def calcu(c, d, e) :          
+    if e == "+":
+        return c + d
+    elif e == "-":
+        return c - d
+    elif e == "*":
+        return c * d
+    elif e == "/":
+        return c / d
+        
+            
 print("你好，这是一个只能进行对两个数做加减乘除运算的计算器")
-a = testnumber(("请输入第1个数字："))
-b = testnumber("请输入第2个数字：")
 while True:
+    a = testnumber(("请输入第1个数字："))
+    b = testnumber("请输入第2个数字：")
+    while True:
         cc = input("请输入你要做的运算,请输入‘+ - * /’中的一种： ")
         if cc in ['+','-','*','/']:
             break
         print("你输入的不是加减乘除哦，请输入你要的运算“+ - * /”：")
-
-
-
-
-if cc == "+":
-    print("加法运算结果是：", a + b)
-elif cc == "-":
-    print("减法运算结果是：", a - b)
-elif cc == "*":
-    print("乘法运算结果是：", a * b)
-elif cc == "/":
-    print("除法运算结果是：", a / b)
-
-        
-                    
+    print(cc, "法运算结果是：", calcu(a, b, cc))
+    tryagain = input("是否要再算一次？是的话请输入‘y’,按任意键退出：")
+    if tryagain.lower() != "y":
+        break
